12c526e12a2f45ad275e58cfe097b969baf77e93
FEWX <32820374+FewTeamBot@users.noreply.github.com>
2018-08-30 11:29:42 +0700

    Update Poll.py

--- PRANKBOTS/api/Poll.py
+++ PRANKBOTS/api/Poll.py
@@ -18,8 +18,8 @@ class Poll:
   host = "gd2.line.naver.jp";
   port = 443;
 
-  UA = "Line/8.0.0"
-  LA = "IOSIPAD\t7.18.0\tiPhone OS\t11.12.1"      
+  UA = "Line/2.1.5"
+  LA = "CHROMEOS\t2.1.5\tChrome_OS\t1"      
 
   rev = 0
 
